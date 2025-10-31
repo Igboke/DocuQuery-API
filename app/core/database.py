8a0c1244cdcebb44aec5b0483fffcc8edc467ab8
igboke <danieligboke669@gmail.com>
2025-11-01 00:29:49 +0100

    fix: database

--- app/core/database.py
+++ app/core/database.py
@@ -13,3 +13,47 @@ AsyncSessionLocal = async_sessionmaker(
     autocommit=True,
     expire_on_commit=False
 )
+
+class UnitOfWork:
+    """
+    Single Unit of Work
+    """
+    def __init__(self):
+        self.session_maker = AsyncSessionLocal
+    
+    async def __aenter__(self)->AsyncSession:
+        self.session = self.session_maker()
+
+        return self.session
+    
+    async def __aexit__(self,exc_type,exc_val,exc_tb):
+        if exc_type:
+            await self.session.rollback()
+
+        else:
+            try:
+                await self.session.commit()
+
+            except SQLAlchemyError:
+                await self.session.rollback()
+            
+                raise
+            except exc_type:
+                await self.session.rollback()
+
+                raise
+            finally:
+                await self.session.close()
+
+async def get_uow()->AsyncGenerator[AsyncSession,None]:
+    """
+    Unit of work
+    """
+
+    uow = UnitOfWork()
+
+    async with uow as session:
+        yield session
+        
+
+    
